fb2cde3baa53b90d2844b22a0bd902206bb2b1dc
Alexander Shelomanov <sasha1998god@yandex.ru>
2019-01-16 15:03:23 +0300

    1 releas

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+var btn = document.querySelector('.E-mail'), // Главная кнопка
+    modal = document.querySelector('.modal'), // Модальное окно
+    closeBtn = document.querySelector('.closeBtn'); // Кнопка, закрывающая модальное окно
+
+// Открытие модального окна
+btn.addEventListener('click', function() {
+    modal.style.display = 'flex'; 
+})
+// Закрытие модального окна при клике на кнопку
+closeBtn.addEventListener('click', function () {
+	modal.style.display = "none";
+})
+// Закрытие модального окна при клике на серую область
+window.addEventListener('click', function (e) {
+	if(e.target == modal) {
+   		modal.style.display = "none";
+	}  	
+})
